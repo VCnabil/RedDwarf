991dbd290ecdcf29331969e3787220bed09fb62a
VCnabil <nlamriben@vcinc.com>
2024-04-24 13:40:03 -0400

    Robust COMMBIV has its own MBIV_RX object and updates its values when message is received  and sends an event with the object . i just need to listen to it and update what i need . i can grab full arrays of values from it . the DATA_tx must be newed on a form, updated by ui , then passed as an argument to Write(DATA). it needs to be sent constantly though

---
+++ RedAwarf/_DataObjz/DataCOMM/DATA_TX.cs
@@ -0,0 +1,304 @@
+﻿using RedDwarf.RedAwarf._Globalz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedDwarf.RedAwarf._DataObjz.DataCOMM
+{
+    public class DATA_TX
+    {
+
+        int _dio; //digtal io
+
+        int _pb; //port bucker
+        int _pn; //port nozzle
+        int _pi; //port interceptor
+        int _sb; //starboard bucker
+        int _sn; //starboard nozzle
+        int _si; //starboard interceptor
+
+        int _pe; //port engine
+        int _se; //starboard engine
+
+        int _sa; //safety
+
+        //make syre the value is smaller than 8 when setting DIO  and greater than 0
+
+
+        public int DIO_0
+        {
+            get { return _dio; }
+            private set
+            {
+                if (value < 0)
+                {
+                    _dio = 0;
+                }
+                else if (value > 8)
+                {
+                    _dio = 8;
+                }
+                else
+                {
+                    _dio = value;
+                }
+            }
+        }
+
+        public void SetDIO(bool arg_bit0, bool arg_bit1, bool arg_bit2, bool argsafe)
+        {
+            _dio = 0;
+            if (arg_bit0)
+            {
+                _dio += 1;
+            }
+            if (arg_bit1)
+            {
+                _dio += 2;
+            }
+            if (arg_bit2)
+            {
+                _dio += 4;
+            }
+
+            _sa = argsafe ? 1 : 0;
+        }
+        public int PB_1
+        {
+            get { return _pb; }
+            set
+            {
+                if (value < 0)
+                {
+                    _pb = 0;
+                }
+                else if (value > 200)
+                {
+                    _pb = 200;
+                }
+                else
+                {
+                    _pb = value;
+                }
+            }
+        }
+        public int PN_2
+        {
+            get { return _pn; }
+            set
+            {
+                if (value < 0)
+                {
+                    _pn = 0;
+                }
+                else if (value > 200)
+                {
+                    _pn = 200;
+                }
+                else
+                {
+                    _pn = value;
+                }
+            }
+        }
+        public int PI_3
+        {
+            get { return _pi; }
+            set
+            {
+                if (value < 0)
+                {
+                    _pi = 0;
+                }
+                else if (value > 200)
+                {
+                    _pi = 200;
+                }
+                else
+                {
+                    _pi = value;
+                }
+            }
+        }
+        public int SB_4
+        {
+            get { return _sb; }
+            set
+            {
+                if (value < 0)
+                {
+                    _sb = 0;
+                }
+                else if (value > 200)
+                {
+                    _sb = 200;
+                }
+                else
+                {
+                    _sb = value;
+                }
+            }
+        }
+        public int SN_5
+        {
+            get { return _sn; }
+            set
+            {
+                if (value < 0)
+                {
+                    _sn = 0;
+                }
+                else if (value > 200)
+                {
+                    _sn = 200;
+                }
+                else
+                {
+                    _sn = value;
+                }
+            }
+        }
+        public int SI_6
+        {
+            get { return _si; }
+            set
+            {
+                if (value < 0)
+                {
+                    _si = 0;
+                }
+                else if (value > 200)
+                {
+                    _si = 200;
+                }
+                else
+                {
+                    _si = value;
+                }
+            }
+        }
+        public int PE_7
+        {
+            get { return _pe; }
+            set
+            {
+                if (value < 0)
+                {
+                    _pe = 0;
+                }
+                else if (value > 2000)
+                {
+                    _pe = 2000;
+                }
+                else
+                {
+                    _pe = value;
+                }
+            }
+        }
+        public int SE_8
+        {
+            get { return _se; }
+            set
+            {
+                if (value < 0)
+                {
+                    _se = 0;
+                }
+                else if (value > 2000)
+                {
+                    _se = 2000;
+                }
+                else
+                {
+                    _se = value;
+                }
+            }
+        }
+        public int SA_9
+        {
+            get { return _sa; }
+            private set
+            {
+                if (value < 0)
+                {
+                    _sa = 0;
+                }
+                else if (value > 1)
+                {
+                    _sa = 1;
+                }
+                else
+                {
+                    _sa = value;
+                }
+            }
+        }
+
+        public DATA_TX()
+        {
+
+            _dio = 0;
+            _pb = 100;
+            _pn = 100;
+            _pi = 100;
+            _sb = 100;
+            _sn = 100;
+            _si = 100;
+            _pe = 2000;
+            _se = 2000;
+            _sa = 1;
+
+        }
+        ~DATA_TX() { }
+
+        public string CREATE_FullString_for_TX()
+        {
+            string formattedStringBODY = Helpers_FormatData(_dio, _pb, _pn, _pi, _sb, _sn, _si, _pe, _se, _sa == 1);
+            return formattedStringBODY;
+        }
+        string Helpers_FormatData(int argDio, int argPb, int argPN, int argPI, int argSB, int argSN, int argSI, int argP, int argS, bool argb)
+        {
+            // Ensure values do not exceed their maximum allowed values
+            argPb = Math.Min(argPb, 255);
+            argPN = Math.Min(argPN, 255);
+            argPI = Math.Min(argPI, 255);
+            argSB = Math.Min(argSB, 255);
+            argSN = Math.Min(argSN, 255);
+            argSI = Math.Min(argSI, 255);
+
+            argP = Math.Min(argP, 2000);
+            argS = Math.Min(argS, 2000);
+
+            // Convert bool to int for string formatting (assuming 1 for true and 0 for false)
+            int boolAsInt = argb ? 1 : 0;
+
+            // Create the formatted string
+            string formattedStringBODY = $"$AAAA,{argDio},{argPb},{argPN},{argPI},{argSB},{argSN},{argSI},{argP},{argS},{boolAsInt}";
+
+            string checksum = Helpers_Generate_Checksum_fromBody(formattedStringBODY);
+            string formattedString = $"{formattedStringBODY}*{checksum}";
+            return formattedString;
+        }
+        string Helpers_Generate_Checksum_fromBody(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return "";
+            short csum = 0;
+            byte[] strg;
+            int idx = 1;  // Start after the '$' character to compute checksum correctly
+
+            strg = Encoding.ASCII.GetBytes(input);
+
+            //while not end of string only 
+            while (idx < strg.Length)
+            {
+                csum ^= strg[idx];  // XOR each byte with the checksum
+                idx++;
+            }
+            return csum.ToString("X2");  // Convert the checksum to a hex string
+        }
+    }
+
+}
---
+++ RedAwarf/_Globalz/MNGR_COMMBIV.cs
@@ -0,0 +1,240 @@
+﻿using RedDwarf.RedAwarf._DataObjz.DataCOMM;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace RedDwarf.RedAwarf._Globalz
+{
+    public class MNGR_COMMBIV : IDisposable
+    {
+        private static readonly Lazy<MNGR_COMMBIV> _instance = new Lazy<MNGR_COMMBIV>(() => new MNGR_COMMBIV());
+        private SerialPort serialPort;
+        private StringBuilder incomingDataBuffer;
+        private string lastCompleteMessage = string.Empty;
+        private string latestComplete_Validated_MessageBody = string.Empty;
+        private string latestCompleteMessageExtratedCHecksum = string.Empty;
+        public static MNGR_COMMBIV Instance { get { return _instance.Value; } }
+        public delegate void MessageReceivedHandler(MBIV_RX  message);
+        public event MessageReceivedHandler MessageReceived;
+        private bool disposed = false;
+
+        MBIV_RX _myMBIV_RX;
+
+        private Timer commTimer;
+        public delegate void TimerElapsedHandler(bool argPortStatus);
+        public event TimerElapsedHandler TimerElapsed;
+        private MNGR_COMMBIV() {
+            incomingDataBuffer = new StringBuilder();
+            serialPort = new SerialPort
+            {
+                //PortName = "COM7", // "COM7 is default here on my pc for serial to MBIV serial connection. but it can be over written later with OpenPort(portname)"
+                BaudRate = 19200,
+                Parity = Parity.None,
+                StopBits = StopBits.One,
+                DataBits = 8,
+                Handshake = Handshake.None
+            };
+            _myMBIV_RX = new MBIV_RX();
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(SerialPort_DataReceived);
+
+            commTimer = new Timer(100); // Set interval to 100 milliseconds
+            commTimer.Elapsed += OnTimerElapsed;
+            commTimer.AutoReset = true;
+            commTimer.Enabled = true;
+            StartTimer();
+        }
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            
+            if (serialPort == null) {
+                TimerElapsed?.Invoke(false);
+            }
+            else if (serialPort.IsOpen)
+            {
+                TimerElapsed?.Invoke(true);
+            }
+            else
+            {
+                TimerElapsed?.Invoke(false);
+            }
+          
+        }
+
+         void StartTimer()
+        {
+            commTimer.Start();
+        }
+
+         void StopTimer()
+        {
+            commTimer.Stop();
+        }
+
+        ~MNGR_COMMBIV()
+        {
+            Dispose(false);
+        }
+        protected virtual void OnMessageReceived(MBIV_RX message)
+        {
+            MessageReceived?.Invoke(message);
+        }
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            incomingDataBuffer.Append(sp.ReadExisting());
+            ProcessBuffer();
+        }
+        private void ProcessBuffer() {
+            // This regex matches the pattern of a complete message ending with *XX where XX are hex digits
+            var regex = new Regex(@"\*[\dA-Fa-f]{2}");
+            string buffer = incomingDataBuffer.ToString();
+            Match match = regex.Match(buffer);
+            // Initialize the last complete message to an empty string at the start of processing
+            string mostRecentMessage = string.Empty;
+
+
+            while (match.Success)
+            {
+                // Extract the complete message up to the end of the checksum
+                int endIndex = match.Index + match.Length;
+                string completeData = buffer.Substring(0, endIndex);
+                // Update the most recent message
+                mostRecentMessage = completeData;
+                // Log or process the complete message
+
+                //OnMessageReceived(mostRecentMessage);  // Fire the event
+                // Prepare for the next message
+                buffer = buffer.Substring(endIndex);
+                match = regex.Match(buffer);
+            }
+
+            // Keep the last complete message
+            if (!string.IsNullOrEmpty(mostRecentMessage))
+            {
+                lastCompleteMessage = mostRecentMessage;
+
+                // Extract the message body and checksum
+                int indexDollar = mostRecentMessage.IndexOf("$");//at index 1 
+                int index = mostRecentMessage.IndexOf("*");//at indesx 128
+                                                           //EventsManagerLib.Call_LogConsole("2. Last complete message: " + mostRecentMessage + "has a $ at index: " + indexDollar + " and * at index: " + index);
+
+                if (indexDollar > 0 && index > indexDollar + 1)
+                {
+                    latestComplete_Validated_MessageBody = mostRecentMessage.Substring(indexDollar, index - indexDollar);
+                    latestCompleteMessageExtratedCHecksum = mostRecentMessage.Substring(index + 1);
+                    //  EventsManagerLib.Call_LogConsole("3. Last complete message body: " + latestComplete_Validated_MessageBody + " and checksum: " + latestCompleteMessageExtratedCHecksum);
+                    bool isChecksumValid = Helpers_Vealidate_Checksum(latestComplete_Validated_MessageBody, latestCompleteMessageExtratedCHecksum);
+                    if (isChecksumValid)
+                    {
+                        //     EventsManagerLib.Call_LogConsole("4. Last complete message: " + mostRecentMessage + " has a valid checksum");
+                        _myMBIV_RX.Update_INTarra_FromCommaDelimitedString(latestComplete_Validated_MessageBody);
+                        OnMessageReceived(_myMBIV_RX);
+                    }
+                    else
+                    {
+                        EventsManagerLib_Call_LogConsole("4. Last complete message: " + mostRecentMessage + " has an invalid checksum");
+                    }
+
+                }
+                else
+                {
+                    EventsManagerLib_Call_LogConsole("4. Last complete message: " + mostRecentMessage + " has no $ or *");
+                }
+            }
+            // Keep the unprocessed part in the buffer
+            incomingDataBuffer.Clear();
+            incomingDataBuffer.Append(buffer);
+        }
+        bool Helpers_Vealidate_Checksum(string argMessageBody, string arg_receivedChecksum)
+        {
+
+            string __calculatedChecksum = Helpers_Generate_Checksum_fromBody(argMessageBody);
+            if (__calculatedChecksum != arg_receivedChecksum)
+            {
+                return false;
+            }
+            return true;
+        }
+        string Helpers_Generate_Checksum_fromBody(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return "";
+            short csum = 0;
+            byte[] strg;
+            int idx = 1;  // Start after the '$' character to compute checksum correctly
+
+            strg = Encoding.ASCII.GetBytes(input);
+
+            //while not end of string only 
+            while (idx < strg.Length)
+            {
+                csum ^= strg[idx];  // XOR each byte with the checksum
+                idx++;
+            }
+            return csum.ToString("X2");  // Convert the checksum to a hex string
+        }
+
+        void EventsManagerLib_Call_LogConsole(string argstr) { 
+            Console.WriteLine(argstr);
+        }
+        public void OpenPort(string portName)
+        {
+            if (!serialPort.IsOpen)
+            {
+                serialPort.PortName = portName;
+                serialPort.Open();
+            }
+        }
+        public void ClosePort()
+        {
+           if (serialPort.IsOpen){
+                serialPort.Close();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (serialPort != null)
+                    {
+                        serialPort.Close();
+                        serialPort.Dispose();
+                    }
+                    if (commTimer != null)
+                    {
+                        StopTimer();
+                        commTimer.Dispose();
+                    }
+                }
+                disposed = true;
+            }
+        }
+
+        public void WriteData(DATA_TX argDatatxObj)
+        {
+
+            string data = argDatatxObj.CREATE_FullString_for_TX();
+            if (serialPort.IsOpen)
+            {
+                serialPort.Write(data);
+            }
+            else
+            {
+                throw new InvalidOperationException("Attempt to write to closed serial port.");
+            }
+        }
+    }
+}
